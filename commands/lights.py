1354ca03f006697a17f2843f1d699198a1f75e7e
n8many <n8nyoung@gmail.com>
2014-01-06 11:26:12 -0500

    "changed files"

--- commands/lights.py
+++ commands/lights.py
@@ -1,6 +1,30 @@
-import command
+from commands.command import Command
 import beautifulhue
+
+name = 'Lights'
 
 class Lights(Command):
     def __init__(self):
-        keywords= ['light','lights']
+        self.keywords = ['light','lights']
+
+    def run(self, commandlist):
+        if len(commandlist) == 0:
+            print "light toggle"
+        elif commandlist[0] == "on":
+            print "lights on"
+        elif commandlist[0] == "off":
+            print "lights off"
+        elif commandlist[0] == "set":
+            print "set tree"
+        elif commandlist[0] == "color":
+            print "color control"
+        elif commandlist[0] == "dim":
+            print "brightness dim"
+        elif commandlist[0] == "bright":
+            print "brightness high"
+        elif commandlist[0] == "brightness":
+            print "brightness level"
+        elif commandlist[0] == "":
+            print "empty command"
+        else:
+            print "err"    
--- interp.py
+++ interp.py
@@ -1,18 +1,48 @@
-from commands import *
+import glob
 
-def interpret(cmdstring):
+class Interp(object):
+    def __init__(self):
+        self.cmdmodules = dict()
+        self.findCommands()
 
-    words = cmdstring.split()
-    commands = list()
-    for w in words:
-        for key in ['and', 'And', 'also', 'Also']:
-            if w == key:
-                commands.append (words[0:words.index(key)])
-                words = words[words.index(key)+1:]
-                break
-    commands.append(words)
+    def importCommand(self, commandName):
+        command = __import__(commandName)
+        for c in commandName.split(".")[1:]:
+            command = getattr(command, c)
+        return command
 
-    for order in commands:
-        if command.isKeyword(order[0]):
-            command.execute(order[1:])
+    def findCommands(self):
+        cmdfiles = glob.glob("commands/*py")
+        for module in cmdfiles:
+            module = module.split("/")[-1].split(".")[0]
+            self.cmdmodules[module] = self.importCommand("commands." +
+                   module)
+        self.cmdmodules.__delitem__('command')
+        self.cmdmodules.__delitem__('__init__')
 
+    def interpret(self, cmdstring):
+
+        words = cmdstring.split()
+        commands = list()
+        for w in words:
+            for key in ['and', 'And', 'also', 'Also']:
+                if w == key:
+                    commands.append (words[0:words.index(key)])
+                    words = words[words.index(key)+1:]
+                    break
+        commands.append(words)
+
+        lastcmd = ''
+        for order in commands:
+            contains = False
+            for command in self.cmdmodules:
+                module = self.cmdmodules[command]  
+                cmd = module.__dict__[module.name]()
+                if cmd.hasKeyword(order[0]):
+                    contains = True
+                    lastcmd = cmd
+                    cmd.run(order[1:])
+                    break
+            if not contains:
+                if lastcmd != '':
+                    lastcmd.run(order)
