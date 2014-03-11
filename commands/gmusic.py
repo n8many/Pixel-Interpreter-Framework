024d14373841df663d6a6f17e1646adc64fe8bfc
n8many <n8nyoung@gmail.com>
2014-03-11 06:28:43 +0000

    Will actually play music now

--- commands/gmusic.py
+++ commands/gmusic.py
@@ -6,7 +6,7 @@ import os
 
 class Gmusic(Command):
     def __init__(self, credentials):
-        self.keywords = ['music']
+        self.keywords = ['music','playlist','song','artist','album']
         self.gmusic = Mobileclient()
         self.queue = list()
         self.cSong = dict()
@@ -16,6 +16,7 @@ class Gmusic(Command):
         self.defaultPlaylist = credentials['playlist']
         self.updateSongs()
         self.devid = credentials['id']
+        self.player = None 
 
     def stripQueue(self, queue):
         queue2 = list()
@@ -55,112 +56,140 @@ class Gmusic(Command):
         url = self.gmusic.get_stream_url(csid,self.devid)
         with open(os.devnull, 'w') as temp:
             self.player = subprocess.Popen(['mplayer','-cache', '8192',  '-ao', 'alsa', '-vo',
-                'null', '%s' % url], stdin=subprocess.PIPE, stdout=temp)
+                'null', '%s' % url], stdin=subprocess.PIPE, stdout=temp, stderr=temp)
         self.cSong = self.getInfo(csid)
         print 'play' + self.cSong['title']
     
     def pause(self):
         self.isPlaying = False
+        if isinstance(self.player, subprocess.Popen):
+            self.player.terminate()
         print 'pausing'
 
     def nextSong(self):
-        self.playlistIndex += 1
-        if self.qIndex >= len(self.currentPlaylist):
+        self.pause()
+        self.qIndex += 1
+        if self.qIndex >= len(self.queue):
             self.qIndex = 0
-            self.pause()
         else:
             self.play()
 
     def previousSong(self):
+        self.pause()
         self.qIndex -= 1
         if self.qIndex < 0:
             self.qIndex = 0
         self.play()
 
     def rickRoll(self):
-        for song in self.library:
-            if song['title'] == 'Never Gonna Give You Up':
-                self.queue = [song['id']]
-                self.qIndex = 0
-                self.play()
+        q = self.findSong('Never Gonna Give You Up')
+        return q
 
     def findSong(self, songname):
+        q = list()
         for song in self.library:
             if songname in song['title']:
-                self.queue = [song['id']]
-                self.qIndex = 0
-                self.play()
-#               tempplaylist = self.gmusic.get_playlist_songs(self.playlists['Megalist'][0])
-#               random.shuffle(tempplaylist)
-#               self.queue += tempplaylist
+                q = [song['id']]
                 break
+        return q
     
     def findAlbum(self, albumname):
-        tempplaylist = list()
+        q = list()
         for song in self.library:
             if albumname in song['album']:
-                tempplaylist += [song]
-        if len(tempplaylist) > 0:
-            tempplaylist = sorted(tempplaylist, key=lambda k: k['track'])
-            self.queue = self.stripQueue(tempplaylist)
-            self.play()
+                q += [song]
+        if len(q) > 0:
+            q = sorted(q , key=lambda k: k['track'])
+            q = stripQueue(q)
+        return q
 
     def findArtist(self, artistname):
-        tempplaylist = list()
+        q = list()
         for song in self.library:
             if artistname in song['artist']:
-                tempplaylist += [song]    
-        if len(templaylist) > 0:
-            self.queue = stripQueue(tempplaylist)
-            random.shuffle(self.queue)
-            self.qIndex = 0
-            self.play()
+                q += [song]    
+        if len(q) > 0:
+            q = stripQueue(q)
+            random.shuffle(q)
+        return q
 
     def findPlaylist(self, playlistname):
+        q = list()
         for playlist in self.playlists:
             if playlist['name'] == playlistname:
-                tempq = playlist['tracks']
-                self.queue = self.stripQueue(tempq)
-        random.shuffle(self.queue)
-        self.qIndex = 0
-        self.play()
+                q = playlist['tracks']
+                q = self.stripQueue(q)
+        random.shuffle(q)
+        return q
 
     def run(self, commandlist):
-        if len(commandlist) == 0:
-            if self.isPlaying == True:
-                self.pause()
+        command = {'action':'toggle', 'what':[],
+            'method':'replace','shuffle':False}
+        for i in xrange(0,len(commandlist)):
+            if commandlist[i] == 'play':
+                command['action'] = 'play'
+            elif commandlist[i] == "playlist":
+                command['what'] = self.findPlaylist(' '.join(commandlist[i:]))
+                command['action'] = 'play'
+                break
+            elif commandlist[i] == "song":
+                command['what'] = self.findSong(' '.join(commandlist[i:]))
+                command['action'] = 'play'
+                break
+            elif commandlist[i] == 'artist':
+                command['what'] = self.findArtist(' '.join(commandlist[i:]))
+                command['action'] = 'play'
+                break
+            elif commandlist[i] == 'album':
+                command['what'] = self.findAlbum(' '.join(commandlist[i:]))
+                command['action'] = 'play'
+                break
+            elif commandlist[i] == 'pause':
+                command = {'action':'pause'}
+                break
+            elif commandlist[i] == 'skip':
+                command = {'action':'skip'}
+                break   
+            elif commandlist[i] == 'previous' or commandlist[i] == 'back':
+                command = {'action':'previous'}
+            elif commandlist[i] == 'shuffle':
+                command['shuffle'] = True
+            elif commandlist[i] == 'next':
+                command['method'] = 'next'
+            elif commandlist[i] == 'add':
+                command['method'] = 'add'
+
+        if command['action'] == 'toggle':
+            if self.isPlaying:
+                command['action'] = 'pause'
             else:
-                self.play()
-            print "music toggle"
+                command['action'] = 'play'
 
-        elif commandlist[0] == "play":
-            if len(commandlist) == 1:
-                if self.isPlaying == False:
-                    self.play()
-                    print "play music"
-
-            elif commandlist [1] == "playlist":
-                self.findPlaylist(" ".join(commandlist[2:]))
-
-            elif commandlist [1] == "song":
-                self.findSong(" ".join(commandlist[2:]))
-
-            elif commandlist [1] == "artist":
-                self.findArtist(" ".join(commandlist[2:]))
-
-            elif commandlist[1] == "album":
-                self.findAlbum(" ".join(commandlist[2:]))
-            
-            
-        elif commandlist[0] == "pause":
-            if self.isPlaying == True:
-                self.pause()
-
-        elif commandlist[0] == "next":
+        if command['action'] == 'pause':
+            self.pause()
+        elif command['action'] == 'skip':
             self.nextSong()
-
-        elif commandlist[0] == "previous":
+        elif command['action'] == 'previous':
             self.previousSong()
+        elif command['action'] == 'play' or command['shuffle']:
+            a = self.queue[:self.qIndex+1]
+            b = self.queue[self.qIndex+1:]
+            if command['method'] == 'add':
+                q = b + command['what']
+            elif command['method'] == 'next':
+                q = command['what'] + b
+            elif command['method'] == 'replace':
+                if self.isPlaying:
+                    self.pause()
+                a = list()
+                if len(command['what'])>0:
+                    q = command['what']
+                else:
+                    q = self.queue
+                self.qIndex = 0
+            if command['shuffle']:
+                random.shuffle(q)
+            self.queue = a + q
+            if not self.isPlaying:
+                self.play()
 
-        else:
-            print "m err"
